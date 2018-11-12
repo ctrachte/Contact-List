7143f56c25814a0b2aea78f83d27d0e0351436cd
ctrachte <cetrachte@hotmail.com>
2018-11-12 16:16:40 -0600

    add key

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -4,7 +4,11 @@ class ListContacts extends Component {
   render () {
     return (
       <ol className='contact-list'>
-        {}
+        {this.props.contacts.map((contact, index) =>
+          <li key={contact.id}>
+            {contact.name}
+          </li>
+         )}
       </ol>
     )
 
