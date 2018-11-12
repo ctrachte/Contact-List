8c9e9357e8e37b4dbbe6da5423b05197cfb4d1c6
ctrachte <cetrachte@hotmail.com>
2018-11-12 16:26:40 -0600

    convert to stateless functional component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
 
-class ListContacts extends Component {
-  render () {
-    return (
-      <ol className='contact-list'>
-        {this.props.contacts.map((contact, index) =>
-          <li key={contact.id} className='contact-list-item'>
-            <div className='contact-avatar' style={{
-              backgroundImage: `url(${contact.avatarURL})`
-            }}/>
-            <div className='contact-details'>
-              <p>{contact.name}</p>
-              <p>{contact.email}</p>
-            </div>
-            <button className='contact-remove'>
-              remove
-            </button>
-          </li>
-         )}
-      </ol>
-    )
-
-  }
+function ListContacts (props) {
+  return (
+    <ol className='contact-list'>
+      {props.contacts.map((contact, index) =>
+        <li key={contact.id} className='contact-list-item'>
+          <div className='contact-avatar' style={{
+            backgroundImage: `url(${contact.avatarURL})`
+          }}/>
+          <div className='contact-details'>
+            <p>{contact.name}</p>
+            <p>{contact.email}</p>
+          </div>
+          <button className='contact-remove'>
+            remove
+          </button>
+        </li>
+       )}
+    </ol>
+  )
 }
+
+// THE BELOW EXAMPLE IS THE SAME FUNCTIONALITY but using class syntax
+
+// class ListContacts extends Component {
+//   render () {
+//     return (
+//       <ol className='contact-list'>
+//         {this.props.contacts.map((contact, index) =>
+//           <li key={contact.id} className='contact-list-item'>
+//             <div className='contact-avatar' style={{
+//               backgroundImage: `url(${contact.avatarURL})`
+//             }}/>
+//             <div className='contact-details'>
+//               <p>{contact.name}</p>
+//               <p>{contact.email}</p>
+//             </div>
+//             <button className='contact-remove'>
+//               remove
+//             </button>
+//           </li>
+//          )}
+//       </ol>
+//     )
+//
+//   }
+// }
 
 export default ListContacts;
