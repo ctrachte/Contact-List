7caf351bde091a05bf3a8820e3083764cf3bbd80
ctrachte <cetrachte@hotmail.com>
2018-11-12 21:14:08 -0600

    add query functionality pt1

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -3,57 +3,84 @@ import PropTypes from 'prop-types';
 
 
 // stateless functional component
-function ListContacts (props) {
-  return (
-    <ol className='contact-list'>
-      {props.contacts.map((contact, index) =>
-        <li key={contact.id} className='contact-list-item'>
-          <div className='contact-avatar' style={{
-            backgroundImage: `url(${contact.avatarURL})`
-          }}/>
-          <div className='contact-details'>
-            <p>{contact.name}</p>
-            <p>{contact.email}</p>
-          </div>
-          <button className='contact-remove' onClick={ () => props.onDeleteContact(contact) }>
-            Remove
-          </button>
-        </li>
-       )}
-    </ol>
-  )
-}
-
-// allows us to specify specific types passed into component, and if they are required or not
-ListContacts.propTypes = {
-  contacts: PropTypes.array.isRequired,
-  onDeleteContact: PropTypes.func.isRequired
-}
+// function ListContacts (props) {
+//
+//   return (
+//     <ol className='contact-list'>
+//       {props.contacts.map((contact, index) =>
+//         <li key={contact.id} className='contact-list-item'>
+//           <div className='contact-avatar' style={{
+//             backgroundImage: `url(${contact.avatarURL})`
+//           }}/>
+//           <div className='contact-details'>
+//             <p>{contact.name}</p>
+//             <p>{contact.email}</p>
+//           </div>
+//           <button className='contact-remove' onClick={ () => props.onDeleteContact(contact) }>
+//             Remove
+//           </button>
+//         </li>
+//        )}
+//     </ol>
+//   )
+// }
 
 // THE BELOW EXAMPLE IS THE SAME FUNCTIONALITY but using class syntax
 
-// class ListContacts extends React.Component {
-//   render () {
-//     return (
-//       <ol className='contact-list'>
-//         {this.props.contacts.map((contact, index) =>
-//           <li key={contact.id} className='contact-list-item'>
-//             <div className='contact-avatar' style={{
-//               backgroundImage: `url(${contact.avatarURL})`
-//             }}/>
-//             <div className='contact-details'>
-//               <p>{contact.name}</p>
-//               <p>{contact.email}</p>
-//             </div>
-//             <button className='contact-remove'>
-//               remove
-//             </button>
-//           </li>
-//          )}
-//       </ol>
-//     )
-//
-//   }
-// }
+class ListContacts extends React.Component {
+  static PropTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+     this.setState({
+       query:query.trim()
+     })
+  }
+
+  render () {
+    return (
+      <div className='list-contacts'>
+        <div className='list-contacts-top'>
+          <input
+            className='search-contacts'
+            type='text'
+            placeholder='Search Contacts'
+            value={this.state.query}
+            onChange={ (event) => this.updateQuery(event.target.value)}
+          />
+        </div>
+        <ol className='contact-list'>
+          {this.props.contacts.map((contact, index) =>
+            <li key={contact.id} className='contact-list-item'>
+              <div className='contact-avatar' style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}/>
+              <div className='contact-details'>
+                <p>{contact.name}</p>
+                <p>{contact.email}</p>
+              </div>
+              <button className='contact-remove' onClick={ () => this.props.onDeleteContact(contact) } >
+                remove
+              </button>
+            </li>
+           )}
+        </ol>
+      </div>
+
+    )
+
+  }
+  // allows us to specify specific types passed into component, and if they are required or not
+  // ListContacts.propTypes = {
+  //   contacts: PropTypes.array.isRequired,
+  //   onDeleteContact: PropTypes.func.isRequired
+  // }
+}
 
 export default ListContacts;
