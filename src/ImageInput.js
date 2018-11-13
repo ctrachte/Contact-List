359557f2747d0ab4d23e9086926392c0186a686e
Caleb Trachte <cetrachte@hotmail.com>
2018-11-13 14:43:44 -0600

    add inputs to create contact form

--- src/ImageInput.js
+++ src/ImageInput.js
@@ -1,31 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
- const readFileAsDataURL = (file) =>
+
+const readFileAsDataURL = (file) =>
   new Promise(resolve => {
     const reader = new FileReader()
-     reader.onload = (event) => {
+
+    reader.onload = (event) => {
       resolve(event.target.result)
     }
-     reader.readAsDataURL(file)
+
+    reader.readAsDataURL(file)
   })
- const resizeImage = (imageURL, canvas, maxHeight) =>
+
+const resizeImage = (imageURL, canvas, maxHeight) =>
   new Promise(resolve => {
     const image = new Image()
-     image.onload = () => {
+
+    image.onload = () => {
       const context = canvas.getContext('2d')
-       if (image.height > maxHeight) {
+
+      if (image.height > maxHeight) {
         image.width *= maxHeight / image.height
         image.height = maxHeight
       }
-       context.clearRect(0, 0, canvas.width, canvas.height)
+
+      context.clearRect(0, 0, canvas.width, canvas.height)
       canvas.width = image.width
       canvas.height = image.height
-       context.drawImage(image, 0, 0, image.width, image.height)
-       resolve(canvas.toDataURL('image/jpeg'))
+
+      context.drawImage(image, 0, 0, image.width, image.height)
+
+      resolve(canvas.toDataURL('image/jpeg'))
     }
-     image.src = imageURL
+
+    image.src = imageURL
   })
- /**
+
+/**
  * A custom <input> that dynamically reads and resizes image files before
  * submitting them to the server as data URLs. Also, shows a preview of the image.
  */
@@ -35,12 +46,15 @@ class ImageInput extends React.Component {
     name: PropTypes.string,
     maxHeight: PropTypes.number
   }
-   state = {
+
+  state = {
     value: ''
   }
-   handleFileChange = (event) => {
+
+  handleFileChange = (event) => {
     const file = event.target.files[0]
-     if (file && file.type.match(/^image\//)) {
+
+    if (file && file.type.match(/^image\//)) {
       readFileAsDataURL(file).then(originalURL => {
         resizeImage(originalURL, this.canvas, this.props.maxHeight).then(url => {
           this.setState({ value: url })
@@ -50,29 +64,36 @@ class ImageInput extends React.Component {
       this.setState({ value: '' })
     }
   }
-   handleFormReset = () => {
+
+  handleFormReset = () => {
     this.setState({ value: '' })
   }
-   componentDidMount() {
+
+  componentDidMount() {
     this.canvas = document.createElement('canvas')
     this.fileInput.form.addEventListener('reset', this.handleFormReset)
   }
-   componentWillUnmount() {
+
+  componentWillUnmount() {
     this.fileInput.form.removeEventListener('reset', this.handleFormReset)
   }
-   render() {
+
+  render() {
     const { className, name } = this.props
     const { value } = this.state
-     const style = {
+
+    const style = {
       position: 'relative'
     }
-     if (value) {
+
+    if (value) {
       style.backgroundImage = `url("${value}")`
       style.backgroundRepeat = 'no-repeat'
       style.backgroundPosition = 'center'
       style.backgroundSize = 'cover'
     }
-     return (
+
+    return (
       <div className={className} style={style}>
         <input type="hidden" name={name} value={value} />
         <input
@@ -92,4 +113,5 @@ class ImageInput extends React.Component {
     )
   }
 }
- export default ImageInput
+
+export default ImageInput
