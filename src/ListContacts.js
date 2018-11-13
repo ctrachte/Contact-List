453b26771153a9a13e4948cbb58fc9bc924c2b5e
Caleb Trachte <cetrachte@hotmail.com>
2018-11-13 14:25:25 -0600

    add link component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
 import escapeRegExp from 'escape-string-regexp';
 import sortBy from 'sort-by';
 
@@ -74,6 +75,10 @@ class ListContacts extends React.Component {
             value={query}
             onChange={ (event) => this.updateQuery(event.target.value)}
           />
+          <Link
+            to='/create'
+            className='add-contact'
+          >Add Contact</Link>
         </div>
 
         {showingContacts.length !== contacts.length && (
